783fa641113c8283edc721fcd18610b10b649637
Brenno Almeida <brenno.galvao@csn.com.br>
2022-08-10 14:37:08 -0300

    CRUD Scaffolding

---
+++ SaleWeb/Data/SaleWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWeb.Models;
+
+namespace SaleWeb.Data
+{
+    public class SaleWebContext : DbContext
+    {
+        public SaleWebContext (DbContextOptions<SaleWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleWeb.Models.Department> Department { get; set; }
+    }
+}
--- SaleWeb/Startup.cs
+++ SaleWeb/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWeb.Data;
 
 namespace SaleWeb
 {
@@ -33,6 +35,9 @@ namespace SaleWeb
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SaleWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SaleWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
